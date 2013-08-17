355b02b5beeaac072946561c4118b3a59ad80d6a
Gergely Tamas <gergely@hailocab.com>
2013-08-17 13:26:30 +0100

    adding assertion

--- features/step_definitions/calculator_steps.rb
+++ features/step_definitions/calculator_steps.rb
@@ -7,6 +7,6 @@ When /^the calculator is run$/ do
 	raise('Command failed!') unless $?.success?
 end
 
-Then /^the output should be "([^"]*)"$/ do |arg1|
-	pending
+Then /^the output should be "([^"]*)"$/ do |expected_output|
+	@output == expected_output
 end
